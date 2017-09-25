240bec08c8ce0643e187e031c972d205fcb8bfdf
renbin.guo <949409706@qq.com>
2017-09-26 07:05:19 +0800

    add blogproject

---
+++ blogproject/blogproject/urls.py
@@ -0,0 +1,22 @@
+"""blogproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),   ## url是django.conf.urls里面的方法  ,他的第一参数为'^admin/' ,第二个参数为admin.site类的url成员变量
+                                        ## 这里实际上是调用url函数,它的返回值作为urlpatterns这个list的元素
+]
